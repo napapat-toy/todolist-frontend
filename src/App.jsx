fe1c547ae27c6c5ca1036cf871f3036c22e153c2
toytoy08 <toy31327@gmail.com>
2022-02-26 04:53:02 +0700

    done edit and adjust ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import './App.css';
-import { deleteTodolist, getTodolist } from './actions/todolistAction'
+import { deleteTodolist, getTodolist, updateTodolist } from './actions/todolistAction'
 import { useEffect } from 'react';
 import ShowTodo from './components/ShowTodo';
 import { createTodolist } from './actions/todolistAction';
@@ -12,21 +12,36 @@ function App() {
   })
 
   const [allTodo, setAllTodo] = useState([])
+  const [editTodo, setEditTodo] = useState(false)
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    createTodolist(todoData)
+    if (editTodo) {
+      updateTodolist(todoData._id, todoData)
+      setEditTodo(false)
+    } else {
+      createTodolist(todoData)
+    }
+    setTodoData({ header: '', text: '' })
   }
 
   const handleDelete = (id) => {
+    if (editTodo) {
+      setEditTodo(false)
+      setTodoData({ header: '', text: '' })
+    }
     deleteTodolist(id)
   }
 
-  const handleEdit = (id) => {
-    console.log(`Edit : ${id}`);
+  const handleEdit = (todo) => {
+    setEditTodo(true)
+    setTodoData(todo)
   }
 
   const handleClear = () => {
+    if (editTodo) {
+      setEditTodo(false)
+    }
     setTodoData({ header: '', text: '' })
   }
 
@@ -49,7 +64,7 @@ function App() {
             <textarea name="detail" id="detail" value={todoData.text} onChange={(e) => setTodoData({ ...todoData, text: e.target.value })}></textarea>
           </div>
           <div className="btn-group">
-            <button type='submit' className='btn'>Add todo</button>
+            <button type='submit' className='btn'>{editTodo ? 'Edit todo' : 'Add todo'}</button>
             <button type='button' className='btn' onClick={handleClear}>Clear</button>
           </div>
         </form>
@@ -57,7 +72,7 @@ function App() {
 
       <div className="showtodo">
         {allTodo.map((todo) => (
-          <ShowTodo key={todo._id} header={todo.header} text={todo.text} handleEdit={() => handleEdit(todo._id)} handleDelete={() => handleDelete(todo._id)} />
+          <ShowTodo key={todo._id} header={todo.header} text={todo.text} handleEdit={() => handleEdit(todo)} handleDelete={() => handleDelete(todo._id)} />
         ))}
       </div>
 
--- src/components/ShowTodo.jsx
+++ src/components/ShowTodo.jsx
@@ -4,10 +4,12 @@ import './showTodo.css'
 function ShowTodo({ header, text, handleEdit, handleDelete }) {
     return (
         <div className='todo-container'>
+
             <h1>{header}</h1>
-            <p>{text}</p>
+            {text ? <p>{text}</p> : ''}
+
             <button className='edit-btn' onClick={handleEdit}>
-                <img src="https://img.icons8.com/material-sharp/24/000000/edit--v1.png" alt='pencil'/>
+                <img src="https://img.icons8.com/material-sharp/24/000000/edit--v1.png" alt='pencil' />
             </button>
             <button className='delete-btn' onClick={handleDelete}>
                 <img src="https://img.icons8.com/ios-filled/50/000000/x.png" alt='x' />
